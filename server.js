a93351c0b02516991adaf5b4e647999fce43cb40
n4n0byt3 <shivbo1@hotmail.com>
2023-02-20 12:23:48 +0000

    completed the workshop tasks

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const port = 3000;
 
 //TODO
 
+app.get('/musicians' , async (req, res) => {
+    let fetchedMusicians = await Musician.findAll();
+    res.json(fetchedMusicians)
+})
+
 app.listen(port, () => {
     sequelize.sync();
     console.log(`Listening on port ${port}`)
